f1c16898544c04ee0446217bce8062ef6bfc0b7d
Cdk29 <etienne.rolland.cdk@gmail.com>
2023-06-30 12:32:18 +0200

    renv.lock

--- app.R
+++ app.R
@@ -3,9 +3,5 @@
 #
 # shiny::runApp()
 library(golem)
-if (!require(bsicons)) {
-  # Install the package if it's not installed
-  install.packages("bsicons", repos = "http://cran.r-project.org")
-}
-library(bsicons)
+# library(bsicons)
 run_app()
