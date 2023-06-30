4d0933a0097d2ff8a797c90729ab472a5ec257ee
Cdk29 <etienne.rolland.cdk@gmail.com>
2023-06-30 12:26:33 +0200

    installation of bsicons in top head script

--- app.R
+++ app.R
@@ -3,5 +3,9 @@
 #
 # shiny::runApp()
 library(golem)
+if (!require(bsicons)) {
+  # Install the package if it's not installed
+  install.packages("bsicons", repos = "http://cran.r-project.org")
+}
 library(bsicons)
 run_app()
