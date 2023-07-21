53c25af3396a50d08a231d2d398bfed9cfa6b215
Cdk29 <etienne.rolland.cdk@gmail.com>
2023-07-21 16:32:27 +0200

    box are aligned to the real numbers

--- R/mod_fundamental_module_cveR.R
+++ R/mod_fundamental_module_cveR.R
@@ -45,40 +45,30 @@ mod_fundamental_module_cveR_ui <- function(id){
   box <- list(
     bslib::value_box(
       title = "Number of news CVEs",
-      value = 44#,
-      # showcase = bsicons::bs_icon("align-bottom")
-    ),
-    bslib::value_box(
-      title = "CVEs suggested",
-      value = 24,
-      # showcase = bsicons::bs_icon("align-center"),
-      theme_color = "dark"
+      value = textOutput(ns("newsCveCount")),
+      theme_color = "primary"
     ),
     bslib::value_box(
       title = "CVEs archived",
-      value = 256,
-      # showcase = bsicons::bs_icon("handbag"),
+      value = textOutput(ns("archivedCveCount")),
       theme_color = "secondary"
     )
   )
 
-
-  # bslib::page_sidebar(
-  #   title = "cveR",
   bslib::nav_panel(id,
-    sidebar = bslib::sidebar(
-      position = "right",
-      title = "Sidebar controls"),
-    bslib::layout_columns(
-      fill = FALSE,
-      box[[1]], box[[2]], box[[3]]
-    ),
-
-    bslib::layout_columns(
-      cards[[1]], cards[[2]]
-    )
+                   sidebar = bslib::sidebar(
+                     position = "right",
+                     title = "Sidebar controls"),
+                   bslib::layout_columns(
+                     fill = FALSE,
+                     box[[1]], box[[2]]
+                   ),
+                   bslib::layout_columns(
+                     cards[[1]], cards[[2]]
+                   )
   )
 }
+
 
 #' fundamental_module_cveR Server Functions
 #' @noRd
@@ -101,6 +91,10 @@ mod_fundamental_module_cveR_server <- function(id, cve_status_df, all_cves){
     # other code...
     news_cves <- all_cves %>% semi_join(cve_no, by = "Name")
     output$btm_plot <- renderPlot(topic_model_BTM_plot_generation(news_cves, ud_model_file))
+
+    # render counts of CVEs
+    output$newsCveCount <- renderText(nrow(cve_tab1))
+    output$archivedCveCount <- renderText(nrow(cve_tab2))
   })
 }
 
